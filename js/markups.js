1b6f61584310dc529f3303dfad906cf7325e30cc
VeronikaVS90 <vsokolovanika@gmail.com>
2024-08-01 23:50:58 +0300

    upd markups

--- js/markups.js
+++ js/markups.js
@@ -1,3 +1,6 @@
+import state from "./state.js";
+import {getFullTitle} from "./utils.js"
+
 export const renderResult = ({ code, amount, full }) => {
   return `<div class="form-result__item-icon icon">
             <img src="./images/arrow.svg" alt="" />
@@ -13,11 +16,13 @@ export const renderResult = ({ code, amount, full }) => {
           <div class="form-result__item-value">${amount.toFixed(2)}</div>`;
 };
 
-export const renderCurrencyItem = ({code, baseCode, rate = 1}) => {
-  return `<div class="currency-item currency-current">
+export const renderCurrencyItem = ({ code, baseCode, rate = 1 }) => {
+  const isBase = code == baseCode;
+  const full = getFullTitle(state.codes, code);
+  return `<div class="currency-item ${isBase ? "currency-current" : ""}">
                             <div class="currency-titles">
-                              <div class="currency-title">USD</div>
-                              <div class="currency-full">American dollar</div>
+                              <div class="currency-title">${code}</div>
+                              <div class="currency-full">${full}</div>
                             </div>
                             <div class="currency-amount">1.00</div>
                             <div class="currency-action">
